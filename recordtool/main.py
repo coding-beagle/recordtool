8e694aa1b8e3733946d1a05f2c519d79f52e1db9
coding-beagle <nicholas.teague@focusrite.com>
2025-06-19 20:03:30 +0100

    some album stuff done

--- recordtool/main.py
+++ recordtool/main.py
@@ -1,12 +1,56 @@
 import customtkinter as ctk
 
 
+class ButtonWithLabel(ctk.CTkFrame):
+    def __init__(self, *args, width, height, label, save_callback, button_text, **kwargs):
+        super().__init__(*args, width=width, height=height, **kwargs)
+
+        self.columnconfigure((1), weight=1)
+        self.columnconfigure((0, 2), weight=0)
+
+        self.label = ctk.CTkLabel(self, text=label, width=100, anchor="w")
+        self.label.grid(row=0, column=0, padx=(5, 5), pady=5, sticky="W")
+
+        self.entry = ctk.CTkEntry(
+            self, width=360, placeholder_text=f"add {label} here")
+        self.entry.grid(row=0, column=1, padx=(5, 5), pady=5, sticky="E")
+
+        self.button = ctk.CTkButton(
+            self, text=button_text if button_text else "Save", width=40, command=lambda: save_callback(self.entry.get()))
+        self.button.grid(row=0, column=2, padx=(5, 5), pady=5)
+
+
+class Metadata(ctk.CTkFrame):
+    def __init__(self, *args,
+                 width: int = 550,
+                 height: int = 140,
+                 **kwargs):
+        super().__init__(*args, width=width, height=height, **kwargs)
+
+        self.title = ctk.CTkLabel(self, text="Album Metadata:")
+        self.title.place(x=10, y=5)
+
+        self.album_name = ButtonWithLabel(
+            self, width=200, height=30, label="Album Title", save_callback=self.print_name, button_text="Save")
+        self.album_name.place(x=10, y=40)
+
+        self.album_name = ButtonWithLabel(
+            self, width=200, height=30, label="Artist", save_callback=self.print_name, button_text="Save")
+        self.album_name.place(x=10, y=85)
+
+    def print_name(self, text):
+        print(text)
+
+
 class App(ctk.CTk):
     def __init__(self):
         super().__init__()
         self.geometry("600x500")
         self.resizable(False, False)
         self.title("Record Tool")
+
+        self.meta = Metadata(self)
+        self.meta.place(x=25, y=100)
 
 
 app = App()
